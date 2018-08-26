9808d5d6664c18522a17120e3f32d3b09ac67c70
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2018-08-26 14:23:43 -0500

    cleanup imports

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { COMPONENTS } from './components';
 import { SharedModule } from '../shared/shared.module';
-import { CaptionEditComponent } from './components/caption-edit/caption-edit.component';
-import { CaptionAddComponent } from './components/caption-add/caption-add.component';
-import { AdminHomeComponent } from './components/admin-home/admin-home.component';
-import { CategoryManagerComponent } from './components/category-manager/category-manager.component';
-import { CategoryAddComponent } from './components/category-add/category-add.component';
 
 @NgModule({
   imports: [
@@ -16,12 +11,7 @@ import { CategoryAddComponent } from './components/category-add/category-add.com
     SharedModule
   ],
   declarations: [
-    ...COMPONENTS,
-    CaptionEditComponent,
-    CaptionAddComponent,
-    AdminHomeComponent,
-    CategoryManagerComponent,
-    CategoryAddComponent
+    ...COMPONENTS
   ]
 })
 export class AdminModule { }
--- src/app/modules/admin/components/index.ts
+++ src/app/modules/admin/components/index.ts
@@ -1,16 +1,22 @@
-import { DeckManagerComponent } from './deck-manager/deck-manager.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { CaptionEditComponent } from './caption-edit/caption-edit.component';
 import { CaptionAddComponent } from './caption-add/caption-add.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CategoryAddComponent } from './category-add/category-add.component';
+import { CategoryManagerComponent } from './category-manager/category-manager.component';
+import { DeckManagerComponent } from './deck-manager/deck-manager.component';
 
 export const COMPONENTS = [
-  DeckManagerComponent,
-  CaptionEditComponent,
+  AdminHomeComponent,
   CaptionAddComponent,
-  AdminHomeComponent
+  CaptionEditComponent,
+  CategoryAddComponent,
+  CategoryManagerComponent,
+  DeckManagerComponent,
 ];
 
-export * from './deck-manager/deck-manager.component';
-export * from './caption-edit/caption-edit.component';
-export * from './caption-add/caption-add.component';
 export * from './admin-home/admin-home.component';
+export * from './caption-add/caption-add.component';
+export * from './caption-edit/caption-edit.component';
+export * from './category-add/category-add.component';
+export * from './category-manager/category-manager.component';
+export * from './deck-manager/deck-manager.component';
